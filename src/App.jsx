256bb727bd40d876d498066adab44efb33ba0fe3
ggang89 <ggang89@gmail.com>
2024-10-02 03:14:31 +0900

    초기 세팅 => 파일정리, 필요한 컴포넌트 생성

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Editor from "./components/Editor";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Editor />
+      <List />
+    </>
+  );
+}
+
+export default App;
