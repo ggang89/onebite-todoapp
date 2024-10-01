7caa669edbd78e262967808bebff7895726b7721
ggang89 <ggang89@gmail.com>
2024-10-02 04:02:28 +0900

    todo형태 구상해서 mockDate에 넣음

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
+import { useState } from "react";
 import Editor from "./components/Editor";
 import Header from "./components/Header";
 import List from "./components/List";
 import "./App.css"
 
+const mockDate = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React Study",
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: " Study",
+    date: new Date().getTime(),
+  },
+];
+
 function App() {
+  
+  const [todos, setTodos] = useState(mockDate);
   return (
     <div className="App">
       <Header />
