6f435c6f5bbc91eba6a9d627abbb8705bbbf8153
ggang89 <ggang89@gmail.com>
2024-10-02 16:32:56 +0900

    새로운 투두 추가하기 => useRef로 id 생성하는 법 ,enter로 추가하기 ,빈투두 입력 방지

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Editor from "./components/Editor";
 import Header from "./components/Header";
 import List from "./components/List";
-import "./App.css"
+import "./App.css";
 
 const mockDate = [
   {
@@ -20,12 +20,22 @@ const mockDate = [
 ];
 
 function App() {
-  
   const [todos, setTodos] = useState(mockDate);
+  const idRef = useRef(2);
+
+  const onCreate = (content) => {
+    const newTodo = {
+      id: idRef.current++,
+      isDone: false,
+      content: content,
+      date: new Date().getTime(),
+    };
+    setTodos([newTodo, ...todos]);
+  };
   return (
     <div className="App">
       <Header />
-      <Editor />
+      <Editor onCreate={onCreate} />
       <List />
     </div>
   );
--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,9 +1,37 @@
-export default function Editor() {
-  
+import { useState, useRef } from "react";
+
+export default function Editor({ onCreate }) {
+  const [content, setContent] = useState("");
+  const inputRef = useRef();
+
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  };
+  const onSubmit = () => {
+    if (content === "") {
+      inputRef.current.focus(); //빈 input을 추가시도할 경우 input에 focus되도록
+      return;
+    }
+    onCreate(content);
+    setContent("");
+  };
+
+  const onKeydown = (e) => {
+    if (e.keyCode === 13) {
+      onSubmit();
+    }
+  };
+
   return (
-    <div className="Editor" >
-      <input placeholder="새로운 Todo..."/>
-      <button>추가</button>
-   </div>
-  )
+    <div className="Editor">
+      <input
+        ref={inputRef}
+        onKeyDown={onKeydown}
+        value={content}
+        onChange={onChangeContent}
+        placeholder="새로운 Todo..."
+      />
+      <button onClick={onSubmit}>추가</button>
+    </div>
+  );
 }
