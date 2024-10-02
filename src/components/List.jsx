c9299580132ae6fb10c8ced8b4529ba418f42818
ggang89 <ggang89@gmail.com>
2024-10-02 17:55:26 +0900

    search bar 기능 구현 => filter 메서드 사용

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,12 +1,32 @@
 import TodoItem from "./TodoItem";
+import { useState } from "react";
 
 export default function List({ todos }) {
+  const [search, setSearch] = useState("");
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getFilteredDate = () => {
+    if (search === "") {
+      return todos;
+    }
+    return todos.filter((todo) =>
+      todo.content.toLowerCase().includes(search.toLowerCase())
+    );
+  };
+  const filteredTodos = getFilteredDate();
   return (
     <div className="List">
       <h4>Todo List 🍀</h4>
-      <input placeholder="검색어를 입력하세요" />
+      <input
+        value={search}
+        onChange={handleSearch}
+        placeholder="검색어를 입력하세요"
+      />
       <div className="TodosWrapper">
-        {todos.map((todo) => {
+        {filteredTodos.map((todo) => {
           return <TodoItem key={todo.id} {...todo} />;
         })}
       </div>
